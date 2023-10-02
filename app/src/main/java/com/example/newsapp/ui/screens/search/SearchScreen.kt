cc6b49fc3ef156cdb28207f064b40600304bbb0e
Farzin <farzinallahyari1234@gmail.com>
2023-10-02 11:23:45 +0330

    code refactor SearchScreen

--- app/src/main/java/com/example/newsapp/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/search/SearchScreen.kt
@@ -1,13 +1,31 @@
 package com.example.newsapp.ui.screens.search
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.newsapp.data.internet.NetworkResult
+import com.example.newsapp.data.model.home.TopNewsModel
+import com.example.newsapp.navigation.Screens
+import com.example.newsapp.ui.component.NewsItem
+import com.example.newsapp.ui.component.PreLoadAnimation
 import com.example.newsapp.viewmodel.SearchViewModel
 
 @Composable
@@ -16,16 +34,72 @@ fun SearchScreen(navController: NavController) {
 }
 
 @Composable
-fun Search(navController: NavController) {
+fun Search(
+    navController: NavController,
+    searchViewModel: SearchViewModel = hiltViewModel(),
+) {
 
-    Column(
+
+    var newsText by remember {
+        mutableStateOf("")
+    }
+
+
+    var searchNewsList by remember {
+        mutableStateOf<List<TopNewsModel>>(emptyList())
+    }
+
+    var loading by remember {
+        mutableStateOf(false)
+    }
+
+
+    val newsBySearchResult by searchViewModel.newsBySearch.collectAsState()
+    when (newsBySearchResult) {
+        is NetworkResult.Success -> {
+            searchNewsList = newsBySearchResult.data ?: emptyList()
+            loading = false
+        }
+
+        is NetworkResult.Error -> {
+            Log.e("TAG", "newsBySearchResult error : ${newsBySearchResult.message}")
+            loading = false
+        }
+
+        is NetworkResult.Loading -> {
+            loading = true
+        }
+
+    }
+
+
+    LazyColumn(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(bottom = 64.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        SearchSection(navController = navController)
+
+        item {
+            TextFieldSection(searchViewModel) {
+                newsText = it
+            }
+        }
+
+
+        if (loading && newsText.isNotBlank()) {
+            item { PreLoadAnimation() }
+        } else {
+            items(searchNewsList) { news ->
+                NewsItem(news = news) {
+                    navController.navigate(Screens.WebPageScreen.route + "?url=${news.url}")
+                }
+            }
+        }
+
+
 
     }
 }
--- app/src/main/java/com/example/newsapp/ui/screens/search/TextFieldSection.kt
+++ app/src/main/java/com/example/newsapp/ui/screens/search/TextFieldSection.kt
@@ -1,25 +1,19 @@
 package com.example.newsapp.ui.screens.search
 
-import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -32,11 +26,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.newsapp.R
-import com.example.newsapp.data.internet.NetworkResult
-import com.example.newsapp.data.model.home.TopNewsModel
-import com.example.newsapp.navigation.Screens
-import com.example.newsapp.ui.component.NewsItem
-import com.example.newsapp.ui.component.PreLoadAnimation
 import com.example.newsapp.ui.theme.Typography
 import com.example.newsapp.ui.theme.darktext
 import com.example.newsapp.ui.theme.roundedShape
@@ -46,48 +35,18 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-@SuppressLint("StateFlowValueCalledInComposition")
 @Composable
-fun SearchSection(
-    navController: NavController,
-    searchViewModel: SearchViewModel = hiltViewModel(),
+fun TextFieldSection(
+    searchViewModel: SearchViewModel,
+    onTextReady:(String)->Unit
 ) {
 
-    var newsText by remember {
+    var newsText by rememberSaveable {
         mutableStateOf("")
-    }
-
-
-    var searchNewsList by remember {
-        mutableStateOf<List<TopNewsModel>>(emptyList())
-    }
-
-    var loading by remember {
-        mutableStateOf(false)
     }
 
     var searchJob by remember { mutableStateOf<Job?>(null) }
     val courotinScope = rememberCoroutineScope()
-
-
-    val newsBySearchResult by searchViewModel.newsBySearch.collectAsState()
-    when (newsBySearchResult) {
-        is NetworkResult.Success -> {
-            searchNewsList = newsBySearchResult.data ?: emptyList()
-            loading = false
-        }
-
-        is NetworkResult.Error -> {
-            Log.e("TAG", "newsBySearchResult error : ${newsBySearchResult.message}")
-            loading = false
-        }
-
-        is NetworkResult.Loading -> {
-            loading = true
-        }
-
-    }
-
 
     Column {
 
@@ -103,8 +62,9 @@ fun SearchSection(
                 newsText = it
                 searchJob?.cancel()
                 searchJob = courotinScope.launch {
-                    delay(1000) // Wait for 500ms before calling the search API
+                    delay(1000)
                     if (searchViewModel.validateQuery(newsText)) {
+                        onTextReady(newsText)
                         searchViewModel.getNewsBySearch(newsText)
                     }
 
@@ -151,29 +111,6 @@ fun SearchSection(
                 }
             }
         )
-
-
-        if (loading && newsText.isNotBlank()){
-            PreLoadAnimation()
-        }else{
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(800.dp)
-                    .padding(bottom = 64.dp)
-            ) {
-
-                items(searchNewsList) { news ->
-                    NewsItem(news = news){
-                        navController.navigate(Screens.WebPageScreen.route + "?url=${news.url}")
-                    }
-                }
-
-            }
-        }
-
-
     }
-
 
 }
